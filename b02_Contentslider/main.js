ed1d3a79db060006db2a310e241af30d2da86275
Lea Moser <lea.moser@stud.fhgr.ch>
2020-06-08 15:20:36 +0200

    added final contentslider

--- b02_Contentslider/main.js
+++ b02_Contentslider/main.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
     let arrow_left = document.querySelector('.arrowleft')
     let arrow_right = document.querySelector('.arrowright')
     let contentboxen = document.querySelectorAll('.contentbox')
-    console.log(contentboxen)
     //EVENTS
     //EVENTS
     //EVENTS
@@ -33,92 +32,34 @@ document.addEventListener('DOMContentLoaded', function () {
     //FUNCTIONS
     //FUNCTIONS
     let changeContent = (number_of_contentboxes, direction_of_swipe) => {
-        let visibles = [];
+        let active_slide;
         for (let i = 0; i < contentboxen.length; i++) {
             if (contentboxen[i].classList.contains('visible')) {
-                visibles.push(i);
+                active_slide = i;
+                contentboxen[i].classList.remove('visible');
             }
         }
-        let new_image_id;
-        let image_to_remove_id;
-        if (direction_of_swipe == 'left') {
-            new_image_id = goLeft(number_of_contentboxes, visibles)
-            if (new_image_id == number_of_contentboxes) {
-                image_to_remove_id = visibles[0];
-            } else {
-                image_to_remove_id = visibles[1]
-            }
+        let new_slide_id;
+        if (direction_of_swipe == 'right') {
+            let new_slide_id = goRight(number_of_contentboxes, active_slide);
+            contentboxen[new_slide_id].classList.add('visible');
         } else {
-            new_image_id = goRight(number_of_contentboxes, visibles)
-            if (new_image_id == 1) {
-                image_to_remove_id = visibles[1]
-            } else {
-                image_to_remove_id = visibles[0]
-            }
+            let new_slide_id = goLeft(number_of_contentboxes, active_slide);
+            contentboxen[new_slide_id].classList.add('visible');
         }
-        contentboxen[new_image_id].classList.add('visible');
-        contentboxen[image_to_remove_id].classList.remove('visible');
     }
-    let goRight = (number_of_contentboxes, visibles) => {
-        console.log(visibles);
-        if (visibles[1] < number_of_contentboxes - 1) {
-            return visibles[1] + 1;
-        } else if (visibles[1] == number_of_contentboxes - 1 && visibles[0] == number_of_contentboxes - 2) {
+    let goRight = (number_of_contentboxes, active_slide) => {
+        if (active_slide < number_of_contentboxes - 1) {
+            return active_slide + 1;
+        } else {
             return 0;
-        } else {
-            return 1;
         }
     }
-    let goLeft = (number_of_contentboxes, visibles) => {
-        console.log(visibles);
-        if (visibles[0] != 0 && visibles[1] != 0) {
-            return visibles[0] - 1;
-        } else if (visibles[0] == 0 && visibles[1] == 1) {
+    let goLeft = (number_of_contentboxes, active_slide) => {
+        if (active_slide != 0) {
+            return active_slide - 1;
+        } else {
             return number_of_contentboxes - 1;
-        } else {
-            return 1;
         }
     }
-
 }, false);
-
-/*
-    let changeImage = (number_of_images, direction_of_swipe) => {
-        //Aktuelle Position des AKtiven Bildes rausfinden
-        let position_of_active = 0;
-        let position_found = false;
-        while (position_found == false) {
-            if (!images[position_of_active].classList.contains('active')) {
-                position_of_active++;
-            } else {
-                position_found = true;
-            }
-        }
-        //Je nach richtung andere Bedingung
-        let new_image_id;
-        if (direction_of_swipe == 'left') {
-            new_image_id = goLeft(number_of_images, position_of_active);
-        } else {
-            new_image_id = goRight(number_of_images, position_of_active);
-        }
-        //Bild wechseln
-        images.forEach((image) => {
-            image.classList.remove('active');
-        })
-        images[new_image_id].classList.add('active');
-    }
-    let goLeft = (number_of_images, position_of_active) => {
-        if (position_of_active > 0) {
-            return position_of_active - 1;
-        } else {
-            return number_of_images - 1;
-        }
-    }
-    let goRight = (number_of_images, position_of_active) => {
-        if (position_of_active < (number_of_images - 1)) {
-            return position_of_active + 1;
-        } else {
-            return 0;
-        }
-    }
-*/
