55287bb95d53284933e58214bd9b1b62e1c166a9
Lea Moser <lea.moser@stud.fhgr.ch>
2021-03-20 15:31:57 +0100

    updated section c

---
+++ c01_Togglecontent-fullwidth/index.php
@@ -0,0 +1,10 @@
+<?php require_once('../_templates/head.php')?>
+    <article class="togglebox">
+        <h3 class="toggleTitle" data-status="closed">Togglebox 1</h3>
+        <p class="toggleContent"><?php generateTextXl(); ?></p>
+        <h3 class="toggleTitle" data-status="closed">Togglebox 2</h3>
+        <p class="toggleContent"><?php generateTextXl(); ?></p>
+        <h3 class="toggleTitle" data-status="closed">Togglebox 3</h3>
+        <p class="toggleContent"><?php generateTextXl(); ?></p>
+    </article>
+<?php require_once('../_templates/foot.php')?>
---
+++ c02_Togglecontent_grid/index.php
@@ -0,0 +1,60 @@
+<?php require_once('../_templates/head.php')?>
+<article>
+        <div class="box">
+            <h1>Box 1</h1>
+        </div>
+        <div class="details">
+            <h2>Details 1</h2>
+            <p><?php generateTextL(); ?></p>
+        </div>
+        <div class="box">
+            <h1>Box 2</h1>
+        </div>
+        <div class="details">
+            <h2>Details 2</h2>
+            <p><?php generateTextL(); ?></p>
+        </div>
+        <div class="box">
+            <h1>Box 3</h1>
+        </div>
+        <div class="details">
+            <h2>Details 3</h2>
+            <p><?php generateTextL(); ?></p>
+        </div>
+        <div class="box">
+            <h1>Box 4</h1>
+        </div>
+        <div class="details">
+            <h2>Details 4</h2>
+            <p><?php generateTextL(); ?></p>
+        </div>
+        <div class="box">
+            <h1>Box 5</h1>
+        </div>
+        <div class="details">
+            <h2>Details 5</h2>
+            <p><?php generateTextL(); ?></p>
+        </div>
+        <div class="box">
+            <h1>Box 6</h1>
+        </div>
+        <div class="details">
+            <h2>Details 6</h2>
+            <p><?php generateTextL(); ?></p>
+        </div>
+        <div class="box">
+            <h1>Box 7</h1>
+        </div>
+        <div class="details">
+            <h2>Details 7</h2>
+            <p><?php generateTextL(); ?></p>
+        </div>
+        <div class="box">
+            <h1>Box 8</h1>
+        </div>
+        <div class="details">
+            <h2>Details 8</h2>
+            <p><?php generateTextL(); ?></p>
+        </div>
+    </article>
+<?php require_once('../_templates/foot.php')?>
