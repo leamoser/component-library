bb04bfd5f3eda9b913fac1833b8723947b5ab225
Lea Moser <lea.moser@stud.fhgr.ch>
2021-03-20 15:44:21 +0100

    updated section x

---
+++ x01_Mobile_Touch_Position/index.php
@@ -0,0 +1,6 @@
+<?php require_once('../_templates/head.php')?>
+<main></main>
+    <section class="infos">
+        <p>Touch-Position: <span id="output"></span></p>
+    </section>
+<?php require_once('../_templates/foot.php')?>
---
+++ x02_Mobile_Swipe/index.php
@@ -0,0 +1,5 @@
+<?php require_once('../_templates/head.php')?>
+<section class="infobox">
+    <p>Swipe-Direction: <span id="info"></span></p>
+</section>
+<?php require_once('../_templates/foot.php')?>
