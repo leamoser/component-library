85e85d8032119e8d23da1341619bb236baf5ba3b
Lea Moser <lea.moser@stud.fhgr.ch>
2021-03-22 20:26:44 +0100

    part 1 update grid-toggle

--- c02_Togglecontent_grid/index.php
+++ c02_Togglecontent_grid/index.php
@@ -1,60 +1,28 @@
-<?php require_once('../_templates/head.php')?>
-<article>
-        <div class="box">
+<?php require_once('../_templates/head.php') ?>
+    <main>
+        <div class="box" data-content="<?php generateTextS(); ?>" data-id="1">
             <h1>Box 1</h1>
         </div>
-        <div class="details">
-            <h2>Details 1</h2>
-            <p><?php generateTextL(); ?></p>
-        </div>
-        <div class="box">
+        <div class="box" data-content="<?php generateTextS(); ?>" data-id="2">
             <h1>Box 2</h1>
         </div>
-        <div class="details">
-            <h2>Details 2</h2>
-            <p><?php generateTextL(); ?></p>
-        </div>
-        <div class="box">
+        <div class="box" data-content="<?php generateTextS(); ?>" data-id="3">
             <h1>Box 3</h1>
         </div>
-        <div class="details">
-            <h2>Details 3</h2>
-            <p><?php generateTextL(); ?></p>
-        </div>
-        <div class="box">
+        <div class="box" data-content="<?php generateTextS(); ?>" data-id="4">
             <h1>Box 4</h1>
         </div>
-        <div class="details">
-            <h2>Details 4</h2>
-            <p><?php generateTextL(); ?></p>
-        </div>
-        <div class="box">
+        <div class="box" data-content="<?php generateTextS(); ?>" data-id="5">
             <h1>Box 5</h1>
         </div>
-        <div class="details">
-            <h2>Details 5</h2>
-            <p><?php generateTextL(); ?></p>
-        </div>
-        <div class="box">
+        <div class="box" data-content="<?php generateTextS(); ?>" data-id="6">
             <h1>Box 6</h1>
         </div>
-        <div class="details">
-            <h2>Details 6</h2>
-            <p><?php generateTextL(); ?></p>
-        </div>
-        <div class="box">
+        <div class="box" data-content="<?php generateTextS(); ?>" data-id="7">
             <h1>Box 7</h1>
         </div>
-        <div class="details">
-            <h2>Details 7</h2>
-            <p><?php generateTextL(); ?></p>
-        </div>
-        <div class="box">
+        <div class="box" data-content="<?php generateTextS(); ?>" data-id="8">
             <h1>Box 8</h1>
         </div>
-        <div class="details">
-            <h2>Details 8</h2>
-            <p><?php generateTextL(); ?></p>
-        </div>
-    </article>
-<?php require_once('../_templates/foot.php')?>
+    </main>
+<?php require_once('../_templates/foot.php') ?>
--- c02_Togglecontent_grid/main.js
+++ c02_Togglecontent_grid/main.js
@@ -1,15 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-    //Code
-    let target = document.querySelectorAll('.box');
+    //Vars
+    const boxen = document.querySelectorAll('.box');
+    const gridsize = getComputedStyle(document.documentElement).getPropertyValue('--bx-grid');
+    const maincontainer = document.querySelector('main');
 
-    target.forEach((box) => {
-        box.addEventListener('click', () => {
-            let details = box.nextElementSibling;
-            if (!details.classList.contains('show')) {
-                details.classList.add('show');
-            } else {
-                details.classList.remove('show');
-            }
+    //Code
+    let createBox = (text) => {
+        let box = document.createElement('div');
+        box.classList.add('detailbox');
+        let cont = document.createElement('p');
+        cont.innerHTML = text;
+        box.appendChild(cont);
+        return box;
+    }
+    let cleanContainer = () => {
+        document.querySelectorAll('.detailbox').forEach(box => {
+            box.remove();
         })
-    })
+    }
+    let styleBox = (box, id) => {
+        const gridposition = Math.floor(parseInt(id)/(parseInt(gridsize) + 0.1)) + 2
+        box.style.gridColumn = '1/' + (parseInt(gridsize) + 1);
+        box.style.gridRow = gridposition + '/' + gridposition;
+        box.style.height = '100%';
+    }
+    let appendBox = (box, id) =>{
+        styleBox(box, id)
+        cleanContainer();
+        maincontainer.appendChild(box);
+    }
+    let initBoxsizing = () => {
+        boxen.forEach((box) => {
+           box.addEventListener('click', function (){
+               appendBox(createBox(this.dataset.content), this.dataset.id);
+           })
+        })
+    }
+
+    //Init
+    initBoxsizing();
 }, false);
