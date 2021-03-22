1950037ac4e20301bd1f036f7e9471a46cf07893
Lea Moser <lea.moser@stud.fhgr.ch>
2021-03-22 19:23:17 +0100

    updated lightbox

--- d03_Lightbox/index.php
+++ d03_Lightbox/index.php
@@ -1,8 +1,8 @@
-<?php require_once('../_templates/head.php')?>
-<div class="cross gone">
+<?php require_once('../_templates/head.php') ?>
+    <div class="cross gone">
         <div></div>
         <div></div>
     </div>
     <img src="img/IMG_0083.jpeg" alt="image of the cat with the name rose">
     <img src="img/IMG_0729.jpeg" alt="image of bedroom with a nice plant">
-<?php require_once('../_templates/foot.php')?>
+<?php require_once('../_templates/foot.php') ?>
